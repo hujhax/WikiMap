356505146e54973ed1ab168e168238305f0f134f
Peter Rogers <peter.rogers@mtuity.com>
2014-02-17 16:47:57 -0600

    Adding d3/angular code that charts the highs in San Francisco.

--- js/WikiSearch.js
+++ js/WikiSearch.js
@@ -50,3 +50,85 @@ wikiModule.controller('WikiController', ['$scope', '$http', function($scope, $ht
 		wikipediaCallAPI($http, $scope, "action=parse&format=json&page=", "&redirects&prop=text", wikipediaPageShow);
 	};
 }]);
+
+// code for test graph
+wikiModule.directive('ngSparkline', function() {
+  return {
+    restrict: 'A',
+    require: '^ngCity',
+    scope: {
+      ngCity: '@'
+    },
+    template: '<div class="sparkline"><h4>Weather for {{ngCity}}</h4><div class="graph"></div></div>',
+    controller: ['$scope', '$http', function($scope, $http) {
+      $scope.getTemp = function(city) {
+      	var url = "http://api.openweathermap.org/data/2.5/forecast/daily?mode=json&units=imperial&cnt=7&callback=JSON_CALLBACK&q="
+
+      	$http({
+      	    method: 'JSONP',
+      	    url: url + city
+      	  }).success(function(data) {
+      	    var weather = [];
+      	    angular.forEach(data.list, function(value){
+      	      weather.push(value);
+      	    });
+      	    $scope.weather = weather;
+      	  });
+      }
+    }],
+    link: function(scope, iElement, iAttrs, ctrl) {
+      scope.getTemp(iAttrs.ngCity);
+      scope.$watch('weather', function(newVal) {
+        if (newVal) {
+        	scope.highs= _.chain(newVal).pluck("temp").pluck("max").value();
+      		chartGraph(iElement, scope.highs, iAttrs);
+        } // newval
+      }); // watch
+    } // link
+  } // return
+});
+
+
+var chartGraph = function(element, data, opts) {
+	var w = opts.width || 200;
+	var	h = opts.height || 80;
+
+	// chart
+	var graph = d3.select(".graph")
+		.append('svg:svg')
+		.attr('width', w)
+		.attr('height', h)
+		.append('g')
+
+	graph.selectAll('*').remove();
+
+	var x = d3.scale.linear().domain([0, data.length]).range([0, w]);
+	var y = d3.scale.linear().domain([0, d3.max(data)]).range([h, 0]);
+
+	var line = d3.svg.line()
+				.x(function(d,i) { return x(i); })
+				.y(function(d) {return y(d); });
+
+	var xAxis = d3.svg.axis().scale(x).tickSize(-h).tickSubdivide(true);
+	graph.append("svg:g")
+		      .attr("class", "x axis")
+		      .attr("transform", "translate(0," + h + ")")
+		      .call(xAxis);
+
+	var yAxisLeft = d3.svg.axis().scale(y).ticks(4).orient("left");
+	graph.append("svg:g")
+		      .attr("class", "y axis")
+		      .attr("transform", "translate(-25,0)")
+		      .call(yAxisLeft);
+
+	graph.append("svg:path")
+			.attr("d", line(data))
+			.attr('fill', 'none')
+			.attr('stroke-width', '1');
+}
+
+wikiModule.directive('ngCity', function() {
+  return {
+    controller: function($scope) {}
+  }
+});
