756bc0fbff669151be33cbc6a7c6e0d4be054d2e
Peter Rogers <peter.rogers@mtuity.com>
2014-02-19 19:24:08 -0600

    Making it convert the parentData to node and link information, instead of just printing it out.

--- js/StupidDirective.js
+++ js/StupidDirective.js
@@ -11,7 +11,26 @@
         link: function(scope, iElement, iAttrs) {
          // watch for data changes and re-render
           scope.$watch('data', function(newVals, oldVals) {
-            console.log(newVals);
+            var nodes = _.pluck(newVals, "parent");
+
+            // create a link for every pair.
+            var links = _.reduce(newVals, function(memo, d) {
+              memo = memo.concat(
+                  _.map(d.children, function(child) {
+                    return [d.parent, child];
+                  })
+                );
+              return memo;
+            }, []);
+
+            // sort pairs
+            links = _.map(links, function(pair) {
+              return pair.sort;
+            });
+
+            // eliminate duplicates
+            links = _.uniq(links);
+
           }, true);
         }
       };
