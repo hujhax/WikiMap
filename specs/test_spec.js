4c9c17776f16bf57b2f0fa2aeff75df37b512e7f
Peter Rogers <peter.rogers@mtuity.com>
2014-03-01 21:40:28 -0600

    Writing the first test on the WikiMap controller.

--- specs/test_spec.js
+++ specs/test_spec.js
@@ -2,3 +2,31 @@ describe("Dummy test suite", function() {
   it("True should be equal to true.", function() {
     expect(true).toBe(true);
   });
+});
+
+describe("Testing the WikiMap controller", function() {
+    // create a mock app module
+    beforeEach(module('wikiApp'));  
+ 
+    // create a mock wikiAPI service
+    beforeEach(module(function ($provide) {
+     var mockWikiAPI = $provide.value('wikiAPI', { 
+         search: function() {return ['Kitten']}
+     });
+   	}));
+
+    // test the controller
+    describe("WikiSearch", function() {
+        var scope;
+        beforeEach(inject(function($rootScope, $controller) {
+            scope = $rootScope.$new();
+            $controller("WikiSearch", { 
+                $scope: scope
+            });
+        }));
+ 
+        it("Default search text should be 'kitten'", function() {
+            expect(scope.searchText).toBe("Kitten");
+        });
+    });
+});
