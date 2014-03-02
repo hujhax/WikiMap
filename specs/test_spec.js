33957430ac33e7721402cb5f5f70eeaf41e8f1dc
Peter Rogers <peter.rogers@mtuity.com>
2014-03-02 02:10:30 -0600

    Fix tabbing.

--- specs/test_spec.js
+++ specs/test_spec.js
@@ -5,52 +5,52 @@ describe("Dummy test suite", function() {
 });
 
 describe("Testing the WikiMap controller.", function() {
-    var fakeSearchResults = ['Kitten', 'Kittenball', 'Kitten Navidad', 'Kitten with a Whip', 
-                             'Kitten\'s Joy', 'Kittenpants', 'Kittens Reichert', 'Kitten heel',
-                             'Kitten Kong', 'Kittens (band)'];
+  var fakeSearchResults = ['Kitten', 'Kittenball', 'Kitten Navidad', 'Kitten with a Whip', 
+                           'Kitten\'s Joy', 'Kittenpants', 'Kittens Reichert', 'Kitten heel',
+                           'Kitten Kong', 'Kittens (band)'];
 
-    var fakeLinkResults = { Main: ['Amnion', 'Donksoy', 'Persian', 'Governing Council of the Cat Fancy']};
+  var fakeLinkResults = { Main: ['Amnion', 'Donksoy', 'Persian', 'Governing Council of the Cat Fancy']};
 
-    // create a mock app module
-    beforeEach(module('wikiApp'));  
- 
-    // create a mock wikiAPI service
-    beforeEach(module(function ($provide) {
-     var mockWikiAPI = $provide.value('wikiAPI', { 
-         search: function(text, callback) {callback(fakeSearchResults);},
-         links: function(text, callback) {callback(fakeLinkResults);}
-     });
-   	}));
+  // create a mock app module
+  beforeEach(module('wikiApp'));  
 
-    // create the controller
-    var scope;
-    beforeEach(inject(function($rootScope, $controller) {
-        scope = $rootScope.$new();
-        $controller("WikiSearch", { 
-            $scope: scope
-        });
-    }));
-
-    // verify its behavior
-    it("Default search text should be 'Kitten'.", function() {
-        expect(scope.searchText).toBe("Kitten");
+  // create a mock wikiAPI service
+  beforeEach(module(function ($provide) {
+   var mockWikiAPI = $provide.value('wikiAPI', { 
+       search: function(text, callback) {callback(fakeSearchResults);},
+       links: function(text, callback) {callback(fakeLinkResults);}
     });
+ 	}));
 
-    it ("Search results should be provided by the wikiAPI.", function () {
-        expect(scope.searchResults).toBe(fakeSearchResults);
+  // create the controller
+  var scope;
+  beforeEach(inject(function($rootScope, $controller) {
+    scope = $rootScope.$new();
+    $controller("WikiSearch", { 
+        $scope: scope
     });
+  }));
 
-    it ("We should be able to create single-node map data.", function () {
-      scope.createMapData("Kitten");
-      expect(scope.mapData).toEqual([{parent: "Kitten", children: []}]); // arrays require toEqual
-    });
+  // verify its behavior
+  it("Default search text should be 'Kitten'.", function() {
+      expect(scope.searchText).toBe("Kitten");
+  });
 
-    it ("We should be able to expand a node.", function () {
-      scope.createMapData("Kitten");
-      scope.expandNode("Kitten");
+  it ("Search results should be provided by the wikiAPI.", function () {
+      expect(scope.searchResults).toBe(fakeSearchResults);
+  });
 
-      expect(scope.mapData.length).toBe(5);
-      expect(_.chain(scope.mapData).pluck("parent").sortBy().value()).toEqual(_.sortBy(fakeLinkResults.Main.concat("Kitten")));
-      expect(_.chain(scope.mapData).pluck("children").flatten().sortBy().value()).toEqual(_.sortBy(fakeLinkResults.Main));
-    });
+  it ("We should be able to create single-node map data.", function () {
+    scope.createMapData("Kitten");
+    expect(scope.mapData).toEqual([{parent: "Kitten", children: []}]); // arrays require toEqual
+  });
+
+  it ("We should be able to expand a node.", function () {
+    scope.createMapData("Kitten");
+    scope.expandNode("Kitten");
+
+    expect(scope.mapData.length).toBe(5);
+    expect(_.chain(scope.mapData).pluck("parent").sortBy().value()).toEqual(_.sortBy(fakeLinkResults.Main.concat("Kitten")));
+    expect(_.chain(scope.mapData).pluck("children").flatten().sortBy().value()).toEqual(_.sortBy(fakeLinkResults.Main));
+  });
 });
